f3f87287489461d72ffd720b7675b4321baf414b
danalee <eleeje97@gmail.com>
2024-12-11 18:13:27 +0900

    :tada: 프로젝트 세팅

---
+++ src/test/java/com/danaleeband/guessit/GuessitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danaleeband.guessit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
