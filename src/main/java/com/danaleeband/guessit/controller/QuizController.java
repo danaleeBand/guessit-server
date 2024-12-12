3a706b66e091b1e2775751e655999b2582047eb8
stead-yujin <70509403+yujch716@users.noreply.github.com>
2024-12-12 19:04:07 +0900

    :sparkles: swagger 연결

--- src/main/java/com/danaleeband/guessit/controller/QuizController.java
+++ src/main/java/com/danaleeband/guessit/controller/QuizController.java
@@ -2,6 +2,12 @@ package com.danaleeband.guessit.controller;
 
 import com.danaleeband.guessit.model.entity.Quiz;
 import com.danaleeband.guessit.service.QuizService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +26,15 @@ public class QuizController {
         this.quizService = quizService;
     }
 
+    @Operation(summary = "퀴즈 조회", description = "id로 퀴즈 조회")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Quiz.class))),
+        @ApiResponse(responseCode = "404", description = "Quiz not found")
+    })
     @GetMapping("/{id}")
-    public Optional<Quiz> getQuizById(@PathVariable Long id) {
+    public Optional<Quiz> getQuizById(
+        @Parameter(description = "ID of the quiz to be retrieved", required = true)
+        @PathVariable Long id) {
         return quizService.getQuizById(id);
     }
 }
