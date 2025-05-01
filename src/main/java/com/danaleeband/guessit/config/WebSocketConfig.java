13c50f1827ef722cd36a2e72de51853b95113206
danalee <eleeje97@gmail.com>
2025-05-01 17:51:06 +0900

    :sparkles: 웹소켓 기본 설

---
+++ src/main/java/com/danaleeband/guessit/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.danaleeband.guessit.config;
+
+import com.danaleeband.guessit.service.WebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+    private static final Logger log = LoggerFactory.getLogger(WebSocketConfig.class);
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        log.info("Registering websocket handlers");
+        registry.addHandler(webSocketHandler, "/ws").setAllowedOrigins("*");
+    }
+}
