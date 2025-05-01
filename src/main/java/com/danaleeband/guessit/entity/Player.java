697ada42affaf45245c0a327f11e9d78f8be6da2
danalee <eleeje97@gmail.com>
2025-05-01 23:03:16 +0900

    :sparkles: playerId -> id 로 변경

--- src/main/java/com/danaleeband/guessit/entity/Player.java
+++ src/main/java/com/danaleeband/guessit/entity/Player.java
@@ -10,7 +10,7 @@ import org.springframework.data.redis.core.RedisHash;
 public class Player implements Serializable {
 
     @Id
-    private Long playerId;
+    private long id;
     private String nickname;
     private String profileUrl;
 
@@ -22,7 +22,7 @@ public class Player implements Serializable {
         this.profileUrl = profileUrl;
     }
 
-    public void assignId(Long playerId) {
-        this.playerId = playerId;
+    public void assignId(Long id) {
+        this.id = id;
     }
 }
