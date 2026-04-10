69ad0302cb23ef4907e48f1db048bbd082b37612
danalee <eleeje97@gmail.com>
2026-04-10 21:45:31 +0900

    :sparkles: Redis 연결 수정

--- src/main/java/com/danaleeband/guessit/config/RedisConfig.java
+++ src/main/java/com/danaleeband/guessit/config/RedisConfig.java
@@ -2,12 +2,10 @@ package com.danaleeband.guessit.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -19,21 +17,12 @@ public class RedisConfig {
 
     private final ObjectMapper objectMapper;
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(
+        RedisConnectionFactory connectionFactory
+    ) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(connectionFactory);
 
         // Key Serializer
         redisTemplate.setKeySerializer(new StringRedisSerializer());
