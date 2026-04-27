a5457f74b25cb82d5b71a716eeef788daab67945
yujch716 <70509403+yujch716@users.noreply.github.com>
2026-04-27 17:11:09 +0900

    :sparkles: health check

---
+++ src/main/java/com/danaleeband/guessit/global/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.danaleeband.guessit.global.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+}
